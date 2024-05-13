26b4bad227acd59f31e21165a20fc88c450ee227
hartk <12398294+hartk1213@users.noreply.github.com>
2024-05-13 13:23:28 -0700

    Delete NonConfigFiles directory

--- NonConfigFiles/IS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-file_name="shaper_calibrate_"
-current_time=$(date "+%Y-%m-%d-%H-%M-%S")
-
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/klipper_config/ISGraphs/"$file_name"x_"$current_time".png
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/klipper_config/ISGraphs/"$file_name"y_"$current_time".png
-rm /tmp/calibration_data_*.csv
--- NonConfigFiles/led.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $2
-python3 ~/klipper_config/NonConfigFiles/e131/ledDMX.py $1 $2
-
